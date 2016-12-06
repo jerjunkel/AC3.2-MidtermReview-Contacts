2fa2000e60c66aa78b56a5f133dfc138906e4d4a
Louis Tur <louis.tur@gmail.com>
2016-12-06 12:16:45 -0500

    task breakdown example, new playground for review URLRequest

---
+++ URLRequest_Review.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+
+// MARK: Model
+struct PlaceholderPost {
+
+  let userID: Int
+  let id: Int
+  let title: String
+  let body: String
+
+}
+
+let userPostsURL = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+
+
+func getRequestWithURLSession() {
+  
+  
+}
+
+func getRequestWithURLRequest() {
+  
+  
+}
+
+func postRequestWithURLRequest() {
+  
+  
+}
+
+//getRequestWithURLSession()
+//getRequestWithURLRequest()
+//postRequestWithURLRequest()
+
+PlaygroundPage.current.needsIndefiniteExecution = true
